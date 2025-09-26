e626d2dc91d7c522d38ea5f8a94f1701c34c5a64
TakedownChain23 <isaac.e.thomas@gmail.com>
2025-09-27 00:04:49 +1000

    Added functionality to return containers with items that require specific packing, and unassigned items which can be packed anywhere

---
+++ SegregatedPacker/Packer.cs
@@ -0,0 +1,89 @@
+﻿namespace SegregatedPacker
+{
+    public static class Packer
+    {
+        public static (List<List<T>> containers, List<T> unassignedItems) PackSegregatedItems<T>(IEnumerable<T> items, Func<T, T, bool> itemsAreCompatible, int maxContainers = 100)
+            where T : notnull
+        {
+            Dictionary<T, List<T>> incompatibleItemsForItem = [];
+            Dictionary<T, int?> assignedContainerForItem = [];
+            var maxContainer = 0;
+
+            if (!items.Any()) return ([], []);
+
+            List<T> itemsRequiringSegregation = [];
+            List<T> itemsNotRequiringSegregation = [];
+            foreach (var item in items)
+            {
+                var incompatibleItems = items.Where(i => !itemsAreCompatible(i, item));
+                if (incompatibleItems.Any())
+                {
+                    itemsRequiringSegregation.Add(item);
+                }
+                else
+                {
+                    itemsNotRequiringSegregation.Add(item);
+                }
+
+                assignedContainerForItem[item] = null;
+                incompatibleItemsForItem[item] = [.. incompatibleItems];
+            }
+
+            foreach (var item in itemsRequiringSegregation)
+            {
+                if (assignedContainerForItem[item] == null)
+                {
+                    var itemQueue = new Queue<T>();
+
+                    assignedContainerForItem[item] = 0;
+                    itemQueue.Enqueue(item);
+                    while (itemQueue.Count > 0)
+                    {
+                        var nextItem = itemQueue.Dequeue();
+
+                        var incompatableItems = incompatibleItemsForItem[nextItem];
+                        foreach (var incompatableItem in incompatableItems.Where(i => assignedContainerForItem[i] == null))
+                        {
+                            var lowestValidContainer = GetLowestValidContainer(incompatableItem, maxContainers);
+                            
+                            assignedContainerForItem[incompatableItem] = lowestValidContainer;
+                            if (maxContainer < lowestValidContainer) maxContainer = lowestValidContainer;
+
+                            itemQueue.Enqueue(incompatableItem);
+                        }
+                    }
+                }
+            }
+
+            var result = new List<List<T>>(maxContainer);
+            for (int i = 0; i <= maxContainer; i++)
+            {
+                result.Add([]);
+            }
+
+            foreach (var item in itemsRequiringSegregation)
+            {
+                var assignedContainer = assignedContainerForItem[item];
+                if (assignedContainer is int container)
+                {
+                    result[container].Add(item);
+                }
+            }
+
+            return (result, itemsNotRequiringSegregation);
+
+            int GetLowestValidContainer(T item, int maxContainers)
+            {
+                var incompatibleContainers = incompatibleItemsForItem[item].Select(i => assignedContainerForItem[i]).Where(c => c != null).Distinct();
+                var container = 0;
+                while (incompatibleContainers.Contains(container))
+                {
+                    container++;
+                    if (container >= maxContainers) throw new Exception("Items require too many containers. Increase maxContainers if required.");
+                }
+
+                return container;
+            }
+        }
+    }
+}
---
+++ SegregatedPackerTests/PackerTests.cs
@@ -0,0 +1,72 @@
+using SegregatedPacker;
+
+namespace SegregatedPackerTests
+{
+    public class PackerTests
+    {
+        static readonly int[] items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        [TestCaseSource(nameof(PackSegregatedItemsTestCases))]
+        public void PackSegregatedItemsTest(Func<int, int, bool> itemsAreCompatable, int[] expectedUnassignedItems, int expectedContainerCount)
+        {
+            (var containers, var unassignedItems) = Packer.PackSegregatedItems(items, itemsAreCompatable);
+
+            Assert.That(unassignedItems, Is.EquivalentTo(expectedUnassignedItems));
+            Assert.That(containers, Has.Count.EqualTo(expectedContainerCount));
+
+            Assert.That(containers.SelectMany(c => c).Concat(unassignedItems), Is.EquivalentTo(items));
+            foreach (var container in containers)
+            {
+                for (var i = 0; i < container.Count; i++)
+                {
+                    for (var j = i + 1; j < container.Count; j++)
+                    {
+                        Assert.That(itemsAreCompatable(container[i], container[j]));
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<TestCaseData> PackSegregatedItemsTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    (int i1, int i2) => true,
+                    items,
+                    1
+                ).SetName("All items compatible - {2} Container(s)");
+
+                yield return new TestCaseData(
+                    (int i1, int i2) => false,
+                    Array.Empty<int>(),
+                    10
+                ).SetName("All items incompatible - {2} Container(s)");
+
+                yield return new TestCaseData(
+                    (int i1, int i2) => Math.Abs(i2 - i1) != 1,
+                    Array.Empty<int>(),
+                    2
+                ).SetName("Chain incompatibility - {2} Container(s)");
+
+                yield return new TestCaseData(
+                    (int i1, int i2) => i1 > 5 || (i1 + 4) % 5 != i2 && (i1 + 6) % 5 != i2,
+                    new int[] { 5, 6, 7, 8, 9 },
+                    3
+                ).SetName("Loop of five incompatibility - {2} Container(s)");
+
+                yield return new TestCaseData(
+                    (int i1, int i2) => i1 * i2 != 0,
+                    Array.Empty<int>(),
+                    2
+                ).SetName("All incompatible with one item - {2} Container(s)");
+
+                yield return new TestCaseData(
+                    (int i1, int i2) => (i1 / 2 == i2 / 2) && Math.Abs(i1 - i2) == 1,
+                    Array.Empty<int>(),
+                    5
+                ).SetName("Adjacent pairs only - {2} Container(s)");
+            }
+        }
+    }
+}
